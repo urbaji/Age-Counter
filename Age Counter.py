6481549006cbd85651d619df9be77406c5214d56
urbaji <145362879+urbaji@users.noreply.github.com>
2025-02-16 18:11:13 +0500

    Delete Age Counter.py

--- Age Counter.py
+++
@@ -1,9 +0,0 @@
-try:
-    n = int(input("Enter your age: "))
-except ValueError: 
-    print("The number you have entered is not an integer, please try again.")
-else:
-    if n % 2 == 0:
-        print("Your number is an even number.")
-    else:
-        print("Your number is an odd number.")
